459c014e344039481f299e73eb62316226d48b54
SagaLD <sagaliljenrothdickman@gmail.com>
2022-03-31 20:29:23 +0200

    listGraph

--- Prog2/ListGraph.java
+++ Prog2/ListGraph.java
@@ -2,5 +2,43 @@ package Prog2;
 import java.io.Serializable;
 
 public class ListGraph<T> implements Graph<T>, Serializable {
+    private void add(T node){
+        
+    }
     
+    private void connect(T node1, T node2, String name, int weight){
+        
+    }
+    
+    private void setConnectionWeight(T node1, T node2, int weight){
+        
+    }
+    
+    private Set<T> getNodes(){
+        
+    }
+    
+    private Collection<Edge<T>> getEdgesFrom(T node){
+        
+    }
+    
+    private Edge<T> getEdgeBetween(T node1, T node2){
+        
+    }
+    
+    private void disconnect(T node1, T node2){
+        
+    }
+    
+    private void remove(T node){
+        
+    }
+    
+    private boolean pathExists(T from, T to){
+        
+    }
+    
+    private List<Edge<T>> getPath(T from, T to){
+        
+    }
 }
